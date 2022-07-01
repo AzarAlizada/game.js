f690c82b2dc3b3258ceeced3440b23b73a215f66
AzarAlizada <100368486+AzarAlizada@users.noreply.github.com>
2022-07-01 17:18:31 +0400

    Add files via upload

---
+++ js oyun/game.js
@@ -0,0 +1,34 @@
+
+    let rdm=Math.floor(Math.random() * 10)
+    
+    function number (){
+        let value="";
+        for(let i=0; i<=4; i++){
+            let rdm=Math.floor(Math.random() * 10)
+        let pro=prompt("Dogru cavabi tapin");
+            if(rdm == 'value' ){
+                alert("Cavab dogrudur");
+            }else{
+                alert("Cavab yanlisdir");
+            }
+        }
+
+    }
+    
+    
+    let value="";
+
+// for(let i=0; i<=4; i++){
+//     let rdm=Math.floor(Math.random() * 10)
+// let pro=prompt("Dogru cavabi tapin");
+//     if(rdm == 'value' ){
+//         alert("Cavab dogrudur");
+//     }else{
+//         alert("Cavab yanlisdir");
+//     }
+// }
+
+
+
+
+
